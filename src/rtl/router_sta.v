3d1b9ae69c3d374ab8895957955d050b9ac9715d
wy10001 <162076018+wy10001@users.noreply.github.com>
2024-03-08 17:53:08 +0800

    Update router_sta.v

--- src/rtl/router_sta.v
+++ src/rtl/router_sta.v
@@ -1,12 +1,12 @@
 `include "global.v"
 
-module router_sta
+module router_sta_3port
 (
 	input 			clk,
 	input 			rst_n,
 	input 			en,
 
-	input				valid,
+	input			valid,
 	input		[2:0]	router_add,
 	input		[2:0]	dst,
 
@@ -16,41 +16,59 @@ module router_sta
 always@(posedge clk or negedge rst_n) 
 begin
 	if (!rst_n) 
-	begin
 		port <= `EMPTY;
-	end 
-
 	else if (en)
 	begin
 		if(!valid)
-		begin
-			port	<=	`EMPTY;
-		end
+			port <= `EMPTY;
 		else if (router_add[1:0] != dst[1:0])
-		begin
-			if (router_add[1:0] < dst[1:0])
-			begin
-				port <= `OUT_X2_PORT;
-			end
-			else 
-			begin
-				port <= `OUT_X1_PORT;
-			end
-		end
-
+			port <= `OUT_X1_PORT;
+		else if (router_add[2] != dst[2])
+			port <= `OUT_Y1_PORT;
 		else 
-		begin
-			if (router_add[2] == dst[2])
-				port <= `OUT_LOCAL_PORT;
-			else 
-			begin
-				if (router_add[2] < dst[2])
-					port <= `OUT_Y1_PORT;
-			end
-  		end
+			port <= `OUT_LOCAL_PORT;
 	end
 end
 
+endmodule
 
+//////////
+module router_sta_4port
+(
+	input 			clk,
+	input 			rst_n,
+	input 			en,
+
+	input			valid,
+	input		[2:0]	router_add,
+	input		[2:0]	dst,
+
+  	output	reg 	[2:0]	port
+);
+
+always@(posedge clk or negedge rst_n) 
+begin
+	if (!rst_n) 
+		port <= `EMPTY;
+	else if (en)
+	begin
+		if(!valid)
+			port <= `EMPTY;
+		else if (router_add[1:0] != dst[1:0])
+		begin
+			if (router_add[1:0] < dst[1:0])
+				port <= `OUT_X2_PORT;
+			else 
+				port <= `OUT_X1_PORT;
+		end
+		else 
+		begin
+			if (router_add[2] != dst[2])
+				port <= `OUT_Y1_PORT;
+			else 
+				port <= `OUT_LOCAL_PORT;
+  		end
+	end
+end
 
 endmodule
