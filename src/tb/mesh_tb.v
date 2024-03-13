586c414b0601b03d3dbe191340f241b6bc9456fd
CN7S <cyx1324841682@sjtu.edu.cn>
2024-03-13 13:21:09 +0800

    mesh_tb.v update

--- src/tb/mesh_tb.v
+++ src/tb/mesh_tb.v
@@ -37,15 +37,15 @@ initial begin
 	// pe_dst_seq_wire = {24'h0, 24'h1, 24'h2, 24'h3, 24'h4, 24'h5, 24'h6, 24'h7};
 	// pe_flush_wire = {8{1'b1}};
 
-	// bit reverse
-	pe_enable = 0;
-	pe_dbg_mode_wire = {8{1'b1}};
-	pe_send_num_wire = {8{3'b001}};
-	pe_receive_num_wire = {8{3'b001}};
-	pe_rate_wire = 0;
-	pe_mode_wire = {8{4'b0001}};
-	pe_dst_seq_wire = {24'h7, 24'h3, 24'h5, 24'h1, 24'h6, 24'h2, 24'h4, 24'h0};
-	pe_flush_wire = {8{1'b1}};
+	// // bit reverse
+	// pe_enable = 0;
+	// pe_dbg_mode_wire = {8{1'b1}};
+	// pe_send_num_wire = {8{3'b001}};
+	// pe_receive_num_wire = {8{3'b001}};
+	// pe_rate_wire = 0;
+	// pe_mode_wire = {8{4'b0001}};
+	// pe_dst_seq_wire = {24'h7, 24'h3, 24'h5, 24'h1, 24'h6, 24'h2, 24'h4, 24'h0};
+	// pe_flush_wire = {8{1'b1}};
 
 	// //bit rotation
 	// pe_enable = 0;
@@ -112,7 +112,9 @@ initial begin
 	#20 rst_n = 1;
 
 	// enable
-	#200
+	#40
+	pe_flush_wire = {8{1'b0}};
+	#500
 	
 	pe_enable = {8{1'b1}};
 
